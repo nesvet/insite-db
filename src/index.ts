105524a024f85e7ccd8d6c170388b63829a19d67
Eugene Nesvetaev <dev@nesvet.dev>
2024-07-24 11:58:44 +0400

    feat: Finally export * from "mongodb"

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,4 @@ export * from "./connect";
 export * from "./extensions";
 export * from "./types";
 export * from "./utils";
-export {
-	type AnyBulkWriteOperation,
-	Binary,
-	type ChangeStreamDocument,
-	GridFSBucket,
-	ObjectId
-} from "mongodb";
+export * from "mongodb";
