8d67afbad2b271dd388b99e0ffbc601e89593f1a
Eugene Nesvetaev <dev@nesvet.dev>
2025-03-17 16:45:44 +0400

    feat: Drop indexes is now optional

--- src/extensions/Collection.ts
+++ src/extensions/Collection.ts
@@ -35,31 +35,33 @@ declare module "mongodb" {
 }
 
 
-Collection.prototype.ensureIndexes = async function (indexes) {
-	
-	const existingIndexes = await this.indexes();
-	
-	for (let i = 0; i < indexes.length; i++) {
-		const [ specification, options = {} ] = indexes[i];
+Collection.prototype.ensureIndexes = async function (indexes, dropOther = false) {
+	if (dropOther) {
+		const existingIndexes = await this.indexes();
 		
-		for (let j = 0; j < existingIndexes.length; j++) {
-			const existingIndex = existingIndexes[j];
+		for (let i = 0; i < indexes.length; i++) {
+			const [ specification, options = {} ] = indexes[i];
 			
-			if (
-				isDeepStrictEqual(specification, existingIndex.key) &&
-				isDeepStrictEqual(options, omit(existingIndex, [ "v", "key", "name" ]))
-			) {
-				indexes.splice(i--, 1);
-				existingIndexes.splice(j, 1);
-				break;
+			for (let j = 0; j < existingIndexes.length; j++) {
+				const existingIndex = existingIndexes[j];
+				
+				if (
+					isDeepStrictEqual(specification, existingIndex.key) &&
+					isDeepStrictEqual(options, omit(existingIndex, [ "v", "key", "name" ]))
+				) {
+					indexes.splice(i--, 1);
+					existingIndexes.splice(j, 1);
+					break;
+				}
 			}
 		}
-	}
-	
-	await Promise.all([
-		...indexes.map(index => this.createIndex(...index)),
-		...existingIndexes.filter(({ name, v }) => name && name !== "_id_" && (!v || v <= ACTUAL_INDEX_VERSION)).map(({ name }) => this.dropIndex(name!))
-	]);
+		
+		await Promise.all([
+			...indexes.map(index => this.createIndex(...index)),
+			...existingIndexes.filter(({ name, v }) => name && name !== "_id_" && (!v || v <= ACTUAL_INDEX_VERSION)).map(({ name }) => this.dropIndex(name!))
+		]);
+	} else
+		await Promise.all(indexes.map(index => this.createIndex(...index)));
 	
 };
 
